fdfa5786d54aa551ccfa5ced19199bcb876c4c38
wubalubadubdub <beargrillsllc@gmail.com>
2016-06-01 11:33:08 -0400

    plays a sound and displays a notification saying whether connection was lost or established

--- app/src/main/java/com/example/bearg/signalnotifier/ConnectivityReceiver.java
+++ app/src/main/java/com/example/bearg/signalnotifier/ConnectivityReceiver.java
@@ -1,5 +1,6 @@
 package com.example.bearg.signalnotifier;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -8,6 +9,7 @@ import android.media.RingtoneManager;
 import android.net.ConnectivityManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 
 /**
  * Created by bearg on 5/31/2016.
@@ -18,17 +20,28 @@ public class ConnectivityReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        boolean networkIsAvailable = intent.getBooleanExtra(
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(context)
+                .setSmallIcon(android.R.drawable.ic_menu_help)
+                .setContentTitle("Connection status changed");
+
+        int notificationId = 1;
+
+        Uri notification = RingtoneManager.getDefaultUri(
+                RingtoneManager.TYPE_NOTIFICATION);
+
+        MediaPlayer player = MediaPlayer.create(context, notification);
+
+        boolean networkIsNotAvailable = intent.getBooleanExtra(
                 ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
 
-        if (networkIsAvailable) {
-            Uri notification = RingtoneManager.getDefaultUri(
-                    RingtoneManager.TYPE_NOTIFICATION);
+        String availability = networkIsNotAvailable? "No connection":"Network available";
+        builder.setContentText(availability);
 
-            MediaPlayer player = MediaPlayer.create(context, notification);
-            player.start();
-
-        }
+        player.start();
+        NotificationManager manager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(notificationId, builder.build());
     }
 
 
